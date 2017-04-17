3baedf3db5b7b85427a992b8bcb0bd76299f728d
vasilvalkov <vasil.valkov@gmail.com>
2017-04-17 13:31:55 +0300

    Added unit tests for SimpleMathExam

---
+++ 01-Defensive-Programming-and-Exceptions/Exceptions-Homework.UnitTests/SimpleMathExam_Tests/Check_Should.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Exceptions_Homework.UnitTests.SimpleMathExam_Tests
+{
+    [TestFixture]
+    public class Check_Should
+    {
+        [Test]
+        public void ThrowArgumentException_WhenInvalidProblemsSolvedCount()
+        {
+            var exam = new SimpleMathExam(3);
+
+            Assert.Throws<ArgumentException>(() => exam.Check());            
+        }
+
+        [Test]
+        public void ReturnNewExamResultWithGrade2_WhenNoProblemsSolvedIsZero()
+        {
+            var exam = new SimpleMathExam(0);
+
+            var result = exam.Check();
+
+            Assert.AreEqual(2, result.Grade);
+        }
+
+        [Test]
+        public void ReturnNewExamResultWithGrade4_WhenProblemsSolvedIsOne()
+        {
+            var exam = new SimpleMathExam(1);
+
+            var result = exam.Check();
+
+            Assert.AreEqual(4, result.Grade);
+        }
+
+        [Test]
+        public void ReturnNewExamResultWithGrade6_WhenProblemsSolvedIsTwo()
+        {
+            var exam = new SimpleMathExam(2);
+
+            var result = exam.Check();
+
+            Assert.AreEqual(6, result.Grade);
+        }        
+    }
+}
---
+++ 01-Defensive-Programming-and-Exceptions/Exceptions-Homework.UnitTests/SimpleMathExam_Tests/Constructor_Should.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Exceptions_Homework.UnitTests.SimpleMathExam_Tests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void CreateNewSimpleMathExamObject_WhenThePassedParameterIsValid()
+        {
+            int validProblemsSolved = 5;
+
+            var result = new SimpleMathExam(validProblemsSolved);
+
+            Assert.AreEqual("SimpleMathExam", result.GetType().Name);
+        }
+
+        [Test]
+        public void CorrectlyAssignProperty_WhenThePassedParameterIsValid()
+        {
+            int validProblemsSolved = 5;
+
+            var result = new SimpleMathExam(validProblemsSolved);
+
+            Assert.AreEqual(5, result.ProblemsSolved);
+        }
+
+        [Test]
+        public void ThrowArgumentException_WhenThePassedParameterIsNegative()
+        {
+            int negativeProblemsSolved = -1;
+
+            Assert.Throws<ArgumentException>(() => new SimpleMathExam(negativeProblemsSolved));
+        }
+
+        [Test]
+        public void ThrowArgumentException_WhenThePassedParameterIsGreaterThan10()
+        {
+            int problemsSolved = 15;
+
+            Assert.Throws<ArgumentException>(() => new SimpleMathExam(problemsSolved));
+        }
+    }
+}
