a26a6912303c22b782fb5c9a038de5556a7958c5
vasilvalkov <vasil.valkov@gmail.com>
2017-04-17 11:35:36 +0300

    Added unit tests for CheckPrime method

---
+++ 01-Defensive-Programming-and-Exceptions/Exceptions-Homework.UnitTests/ExceptionsHomework_Tests/PrimeCheck_Should.cs
@@ -0,0 +1,49 @@
+﻿using Exceptions_Homework;
+using NUnit.Framework;
+using System;
+
+namespace Exceptions_Homework.UnitTests.ExceptionsHomework_Tests
+{
+    [TestFixture]
+    public class PrimeCheck_Should
+    {
+        [TestCase(1)]
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(-33)]
+        public void ThrowArgumentException_WhenThePassedNumberIsSmallerThanTwo(int invalidNumber)
+        {
+            Assert.Throws<ArgumentException>(() => ExceptionsHomework.CheckPrime(invalidNumber));
+        }
+
+        [TestCase(2)]
+        [TestCase(7)]
+        [TestCase(1000)]
+        public void NotThrow_WhenThePassedNumberIsValid(int validNumber)
+        {
+            Assert.DoesNotThrow(() => ExceptionsHomework.CheckPrime(validNumber));
+        }
+
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(23)]
+        [TestCase(113)]
+        public void ReturnTrue_WhenThePassedNumberIsPrime(int primeNumber)
+        {
+            bool isPrime = ExceptionsHomework.CheckPrime(primeNumber);
+
+            Assert.IsTrue(isPrime);
+        }
+
+        [TestCase(4)]
+        [TestCase(9)]
+        [TestCase(27)]
+        [TestCase(1000)]
+        public void ReturnFalse_WhenThePassedNumberIsNotPrime(int nonprimeNumber)
+        {
+            bool isPrime = ExceptionsHomework.CheckPrime(nonprimeNumber);
+
+            Assert.IsFalse(isPrime);
+        }
+    }
+}
