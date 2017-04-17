fdbd6cabb9a91eb603c1ef216ac1b70bafd292a5
vasilvalkov <vasil.valkov@gmail.com>
2017-04-17 12:27:41 +0300

    Added unit tests for CSharpExam

---
+++ 01-Defensive-Programming-and-Exceptions/Exceptions-Homework.UnitTests/CSharpExam_Tests/Check_Should.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Exceptions_Homework.UnitTests.CSharpExam_Tests
+{
+    [TestFixture]
+    public class Check_Should
+    {
+        [Test]
+        public void ReturnNewExamResult_WhenInvoked()
+        {
+            int validScore = 42;
+            var exam = new CSharpExam(validScore);
+
+            var result = exam.Check();
+
+            Assert.AreEqual("ExamResult", result.GetType().Name);
+        }
+    }
+}
---
+++ 01-Defensive-Programming-and-Exceptions/Exceptions-Homework.UnitTests/CSharpExam_Tests/Constructor_Should.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Exceptions_Homework.UnitTests.CSharpExam_Tests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ThrowArgumentException_WhenThePassedScoreIsNegative()
+        {
+            int negativeScore = -1;
+
+            Assert.Throws<ArgumentException>(() => new CSharpExam(negativeScore));
+        }
+
+        [Test]
+        public void ThrowArgumentException_WhenThePassedScoreIsGreaterThan100()
+        {
+            int invalidScore = 101;
+
+            Assert.Throws<ArgumentException>(() => new CSharpExam(invalidScore));
+        }
+
+        [Test]
+        public void CreateNewCSharpExam_WhenThePassedScoreIsValid()
+        {
+            int validScore = 42;
+
+            var result = new CSharpExam(validScore);
+
+            Assert.AreEqual("CSharpExam", result.GetType().Name);
+        }
+
+        [Test]
+        public void CorrectlyAssignScoreToProperty_WhenThePassedScoreIsValid()
+        {
+            int validScore = 42;
+
+            var result = new CSharpExam(validScore);
+
+            Assert.AreEqual(42, result.Score);
+        }
+    }
+}
